987d5f9693ecf89f30b6aca9aaa2a35e4bf407b7
Martha Mendoza A01284654 <mmarthamendozaa@gmail.com>
2024-05-02 12:07:26 -0600

    exploreadmin
    arreglar exploreadmin

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Explore.css';
-import { AiOutlineSearch } from "react-icons/ai";
 import { getIniciativas } from './Explore-fb.js';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
