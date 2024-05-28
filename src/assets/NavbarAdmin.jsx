8202513224ac85a728b3d1c9cfad867c309df53f
Isabella Garduño <105307704+a00833573@users.noreply.github.com>
2024-05-28 11:00:29 -0600

    Ahora si esta alineado todo
    Me equivoqué antes

--- src/assets/NavbarAdmin.jsx
+++ src/assets/NavbarAdmin.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import './NavbarAdmin.css';
 import { FaCompass, FaUser } from 'react-icons/fa';
 
 const NavbarAdmin = ({ isCreateOpen, toggleCreate }) => {
