9747f4e6f346ea131753e3f3006f3b7b407d9853
Isabella Garduño <105307704+a00833573@users.noreply.github.com>
2024-05-28 14:30:27 -0600

    Tamaño igual del modal

--- src/assets/NavbarAdmin.jsx
+++ src/assets/NavbarAdmin.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './NavbarAdmin.css';
+import './Navbar.css';
 import { FaCompass, FaUser } from 'react-icons/fa';
 
 const NavbarAdmin = ({ isCreateOpen, toggleCreate }) => {
