78a1567dce8cb7505f457ef54bfebac71821f9b4
vale lopez <117028127+vlopezbarce@users.noreply.github.com>
2024-06-12 18:33:36 -0600

    calcular edad desde front

--- src/pages/Initiative/Initiative.jsx
+++ src/pages/Initiative/Initiative.jsx
@@ -33,9 +33,10 @@ export const Initiative = () => {
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const day = date.getDate();
-    const month = date.getMonth() + 1; 
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); 
     const year = date.getFullYear();
+
     return `${day}/${month}/${year}`;
   };
 
